4c54a3e5fcec2b6efafdb07f4123882691da17dc
Gabriel Ochsenhofer <gabs@gabs.tv>
2018-11-19 15:44:31 -0200

    hellow world, moving

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -9,6 +9,9 @@ import (
 
 var engine *groove.Engine
 var hellocomp *ecs.Component
+var movecomp *ecs.Component
+
+const SPEED float64 = 120
 
 func main() {
 	engine = groove.NewEngine(&groove.NewEngineInput{
@@ -20,18 +23,30 @@ func main() {
 	// add components and systems
 	world := engine.Default()
 	comp, err := world.NewComponent(ecs.NewComponentInput{
-		Name: "init_engine",
+		Name: "hello",
 	})
 	if err != nil {
 		panic(err)
 	}
 	hellocomp = comp
-	sys0 := world.NewSystem(0, initEngineSystemExec, comp)
+	movecomp, err = world.NewComponent(ecs.NewComponentInput{
+		Name: "move",
+	})
+	if err != nil {
+		panic(err)
+	}
+	sys0 := world.NewSystem(0, initEngineSystemExec, hellocomp)
 	sys0.AddTag(groove.WorldTagDraw)
+	sys1 := world.NewSystem(1, moveSysExec, movecomp, hellocomp)
+	sys1.AddTag(groove.WorldTagUpdate)
 	entity0 := world.NewEntity()
-	world.AddComponentToEntity(entity0, comp, &initEngineData{"Hello,", 30, 40})
+	world.AddComponentToEntity(entity0, hellocomp, &initEngineData{"Hello,", 30, 40})
 	entity1 := world.NewEntity()
-	world.AddComponentToEntity(entity1, comp, &initEngineData{"World!", 50, 60})
+	world.AddComponentToEntity(entity1, hellocomp, &initEngineData{"World!", 50, 60})
+	world.AddComponentToEntity(entity1, movecomp, &moveCompData{
+		XSpeed: SPEED,
+		YSpeed: SPEED,
+	})
 	// run
 	engine.Run()
 }
@@ -42,10 +57,54 @@ type initEngineData struct {
 	Y    int
 }
 
+type moveCompData struct {
+	XSpeed float64
+	YSpeed float64
+	XSum   float64
+	YSum   float64
+}
+
 func initEngineSystemExec(dt float64, view *ecs.View) {
 	img := engine.Get(groove.EbitenScreen).(*ebiten.Image)
 	for _, v := range view.Matches() {
 		data := v.Components[hellocomp].(*initEngineData)
 		ebitenutil.DebugPrintAt(img, data.Text, data.X, data.Y)
+	}
+}
+
+func moveSysExec(dt float64, view *ecs.View) {
+	for _, v := range view.Matches() {
+		iedata := v.Components[hellocomp].(*initEngineData)
+		movedata := v.Components[movecomp].(*moveCompData)
+		movedata.XSum += dt * movedata.XSpeed
+		movedata.YSum += dt * movedata.YSpeed
+		for movedata.XSum >= 1 {
+			iedata.X++
+			movedata.XSum--
+		}
+		for movedata.XSum <= -1 {
+			iedata.X--
+			movedata.XSum++
+		}
+		for movedata.YSum >= 1 {
+			iedata.Y++
+			movedata.YSum--
+		}
+		for movedata.YSum <= -1 {
+			iedata.Y--
+			movedata.YSum++
+		}
+		if iedata.X >= 280 && movedata.XSpeed > 0 {
+			movedata.XSpeed = -SPEED
+		}
+		if iedata.X <= 0 && movedata.XSpeed < 0 {
+			movedata.XSpeed = SPEED
+		}
+		if iedata.Y >= 220 && movedata.YSpeed > 0 {
+			movedata.YSpeed = -SPEED
+		}
+		if iedata.Y <= 0 && movedata.YSpeed < 0 {
+			movedata.YSpeed = SPEED
+		}
 	}
 }
