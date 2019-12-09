5b757824bcf1602e9986db22a6064f0cd3180c97
Gabriel Ochsenhofer <gabs@gabs.tv>
2019-12-09 11:04:45 -0300

    go doc

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/gabstv/troupe
 
 require (
 	github.com/gabstv/ecs v1.2.3
-	github.com/gopherjs/gopherwasm v1.1.0 // indirect
 	github.com/hajimehoshi/ebiten v1.10.1
 	github.com/stretchr/testify v1.4.0
 )
--- pkg/troupe/ecs.go
+++ pkg/troupe/ecs.go
@@ -8,34 +8,50 @@ import (
 	"github.com/hajimehoshi/ebiten"
 )
 
+// System -> ecs.System
 type System = ecs.System
+
+// Component -> ecs.Component
 type Component = ecs.Component
+
+// View -> ecs.View
 type View = ecs.View
+
+// Entity -> ecs.Entity
 type Entity = ecs.Entity
+
+// NewComponentInput -> ecs.NewComponentInput
 type NewComponentInput = ecs.NewComponentInput
+
+// Worlder -> ecs.Worlder
 type Worlder = ecs.Worlder
 
 type World struct {
 	*ecs.World
 }
 
+// Run all systems
 func (w *World) Run(screen *ebiten.Image, delta float64) (taken time.Duration) {
 	w.Set("screen", screen)
 	return w.World.Run(delta)
 }
 
+// RunWithTag runs all systems with the specified tag
 func (w *World) RunWithTag(tag string, screen *ebiten.Image, delta float64) (taken time.Duration) {
 	w.Set("screen", screen)
 	return w.World.RunWithTag(tag, delta)
 }
 
+// RunWithoutTag runs all systems without the specified tag
 func (w *World) RunWithoutTag(tag string, screen *ebiten.Image, delta float64) (taken time.Duration) {
 	w.Set("screen", screen)
 	return w.World.RunWithoutTag(tag, delta)
 }
 
+// SystemFn is the loop function of a system
 type SystemFn func(ctx Context, screen *ebiten.Image)
 
+// NewSystem creates a new system
 func (w *World) NewSystem(priority int, fn SystemFn, comps ...*Component) *System {
 	fn2 := func(ctx ecs.Context) {
 		scr := w.Get("screen").(*ebiten.Image)
@@ -45,10 +61,12 @@ func (w *World) NewSystem(priority int, fn SystemFn, comps ...*Component) *Syste
 	return w.World.NewSystem(priority, fn2, comps...)
 }
 
+// NewComponent creates a new component
 func (w *World) NewComponent(input NewComponentInput) (*Component, error) {
 	return w.World.NewComponent(input)
 }
 
+// NewWorld creates a new world
 func NewWorld(e *Engine) *World {
 	w := &World{
 		World: ecs.NewWorldWithCtx(func(c0 context.Context, dt float64, sys *System, w Worlder) ecs.Context {
