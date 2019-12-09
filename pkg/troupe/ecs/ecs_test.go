f3cf9ec1661c4365d3527629dab7db275ceae478
Gabriel Ochsenhofer <gabs@gabs.tv>
2019-12-09 06:51:35 -0300

    testing imported ECS

--- pkg/troupe/ecs/ecs_test.go
+++ pkg/troupe/ecs/ecs_test.go
@@ -6,13 +6,29 @@ import (
 	"github.com/hajimehoshi/ebiten"
 )
 
+type c0data struct {
+	Points float64
+	Scale  float64
+}
+
 func TestECS(t *testing.T) {
+	s0 := 0.0
 	w := NewWorld()
 	c0, _ := w.NewComponent(NewComponentInput{
 		Name: "test",
 	})
 	w.NewSystem(0, func(ctx Context, screen *ebiten.Image) {
-		// do nothing
+		matches := ctx.System().View().Matches()
+		d := matches[0].Components[c0].(*c0data)
+		s0 += d.Points * d.Scale * ctx.DT()
 	}, c0)
+	c0arch := NewArchetype(w, c0)
+	c0arch.NewEntity(&c0data{
+		Points: 10,
+		Scale:  0.25,
+	})
 	w.Run(nil, 1.0)
+	if s0 != 2.5 {
+		t.Fail()
+	}
 }
