55f284d1e9ab7005d051b4e596e062e8b3e5e0c5
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-04-26 08:36:42 -0300

    broccoli

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module github.com/gabstv/tau
 
 require (
+	aletheia.icu/broccoli v1.0.3 // indirect
+	aletheia.icu/broccoli/fs v0.0.0-20200420200651-c5ac961a357a // indirect
 	github.com/gabstv/ecs v1.4.3
 	github.com/go-gl/glfw v0.0.0-20190409004039-e6da0acd62b1 // indirect
 	github.com/hajimehoshi/ebiten v1.12.0-alpha.0.20200421172316-7ad3343e9cab
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.5.1
 	github.com/urfave/cli v1.22.2
 )
 
---
+++ io/broccolifs/wrapper.go
@@ -0,0 +1,40 @@
+package broccolifs
+
+import (
+	"aletheia.icu/broccoli/fs"
+	"github.com/gabstv/tau/io"
+)
+
+type ffile struct {
+	*fs.File
+}
+
+func (f *ffile) Stat() (io.FileInfo, error) {
+	fi, err := f.File.Stat()
+	if err != nil {
+		return nil, err
+	}
+	return fi, nil
+}
+
+type wrapper struct {
+	*fs.Broccoli
+}
+
+func (w *wrapper) Open(name string) (io.File, error) {
+	ff, err := w.Broccoli.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return &ffile{
+		File: ff,
+	}, nil
+}
+
+func (w *wrapper) Stat(name string) (io.FileInfo, error) {
+	fi, err := w.Broccoli.Stat(name)
+	if err != nil {
+		return nil, err
+	}
+	return fi, nil
+}
