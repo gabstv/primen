b7683824e6837d75f70a04ab1701d8af2a371e4e
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-04-26 01:53:18 -0300

    fix fs

--- io/os/osfs.go
+++ io/os/osfs.go
@@ -1,7 +1,6 @@
 package os
 
 import (
-	"io"
 	"os"
 	"path"
 
@@ -12,16 +11,34 @@ type osfs struct {
 	basepath string
 }
 
+type ffile struct {
+	*os.File
+}
+
+func (f *ffile) Stat() (io.FileInfo, error) {
+	fi, err := f.File.Stat()
+	if err != nil {
+		return nil, err
+	}
+	return fi, nil
+}
+
 // New returns a new OS filesystem with the base path as root.
 func New(base string) io.Filesystem {
 	return &osfs{base}
 }
 
-func (fs *osfs) Open(name string) (io.ReadCloser, error) {
-	return os.Open(path.Join(fs.basepath, name))
+func (fs *osfs) Open(name string) (io.File, error) {
+	ff, err := os.Open(path.Join(fs.basepath, name))
+	if err != nil {
+		return nil, err
+	}
+	return &ffile{
+		File: ff,
+	}, nil
 }
 
-func (fs *osfs) Stat(name string) (io.File, error) {
+func (fs *osfs) Stat(name string) (io.FileInfo, error) {
 	finfo, err := os.Stat(path.Join(fs.basepath, name))
 	return finfo, err
 }
