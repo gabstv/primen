2a890ba2bf2492eef9c54096501daa2e3356dda5
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-05-16 17:45:27 -0300

    updated layer logic

--- drawlayer.go
+++ drawlayer.go
@@ -3,6 +3,7 @@ package tau
 import (
 	"math"
 	"sort"
+	"sync"
 
 	"github.com/gabstv/ecs"
 	"github.com/hajimehoshi/ebiten"
@@ -53,8 +54,9 @@ const (
 )
 
 type drawLayerDrawer struct {
-	index LayerIndex
-	items *EntitySortedList
+	index    LayerIndex
+	slcindex int
+	items    *EntitySortedList
 }
 
 type drawLayerItemCache struct {
@@ -62,38 +64,63 @@ type drawLayerItemCache struct {
 	Sprite *Sprite
 }
 
-//
-type drawLayerDrawers []*drawLayerDrawer
-
-func (a drawLayerDrawers) Len() int           { return len(a) }
-func (a drawLayerDrawers) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a drawLayerDrawers) Less(i, j int) bool { return a[i].index < a[j].index }
-func (a drawLayerDrawers) Find(index LayerIndex) *drawLayerDrawer {
-	n := sort.Search(len(a), func(i int) bool {
-		return a[i].index <= index
-	})
-	if n == len(a) {
-		return nil
-	}
-	if a[n].index != index {
-		return nil
-	}
-	return a[n]
+func (c *drawLayerItemCache) Less(v interface{}) bool {
+	return c.ZIndex < v.(*drawLayerItemCache).ZIndex
 }
 
 //
-func newDrawLayerDrawer(index LayerIndex) *drawLayerDrawer {
-	return &drawLayerDrawer{
-		index: index,
-		items: NewEntitySortedList(func(ival, jval interface{}) bool {
-			return ival.(*drawLayerItemCache).ZIndex < jval.(*drawLayerItemCache).ZIndex
-		}, func(needleval, haystackitemval interface{}) bool {
-			return haystackitemval.(*drawLayerItemCache).ZIndex <= needleval.(*drawLayerItemCache).ZIndex
-		}, 1024),
-	}
+type drawLayerDrawers struct {
+	l     sync.RWMutex
+	slice []*drawLayerDrawer
+	m     map[LayerIndex]*drawLayerDrawer
 }
 
-//
+func (d *drawLayerDrawers) UpsertLayer(index LayerIndex) *EntitySortedList {
+	d.l.RLock()
+	x := d.m[index]
+	d.l.RUnlock()
+	if x != nil {
+		return x.items
+	}
+	d.l.Lock()
+	defer d.l.Unlock()
+	x = &drawLayerDrawer{
+		index:    index,
+		items:    NewEntitySortedList(1024),
+		slcindex: len(d.slice),
+	}
+	d.m[index] = x
+	d.slice = append(d.slice, x)
+	sort.Sort(drawLayerDrawersL(d.slice))
+	return x.items
+}
+
+type LayerTuple struct {
+	Index LayerIndex
+	Items *EntitySortedList
+}
+
+func (d *drawLayerDrawers) All() []LayerTuple {
+	d.l.RLock()
+	defer d.l.RUnlock()
+	cl := make([]LayerTuple, 0, len(d.slice))
+	for _, v := range d.slice {
+		cl = append(cl, LayerTuple{
+			Index: v.index,
+			Items: v.items,
+		})
+	}
+	return cl
+}
+
+type drawLayerDrawersL []*drawLayerDrawer
+
+func (a drawLayerDrawersL) Len() int { return len(a) }
+func (a drawLayerDrawersL) Swap(i, j int) {
+	a[i].slcindex, a[j].slcindex = j, i
+	a[i], a[j] = a[j], a[i]
+}
+func (a drawLayerDrawersL) Less(i, j int) bool { return a[i].index < a[j].index }
 
 func drawLayerComponentDef(w *ecs.World) *ecs.Component {
 	return UpsertComponent(w, ecs.NewComponentInput{
@@ -122,49 +149,46 @@ func (cs *DrawLayerComponentSystem) SystemPriority() int {
 
 func (cs *DrawLayerComponentSystem) SystemInit() SystemInitFn {
 	return func(w *ecs.World, sys *ecs.System) {
-		layers := drawLayerDrawers(make([]*drawLayerDrawer, 0, 16))
+		layers := &drawLayerDrawers{
+			slice: make([]*drawLayerDrawer, 0, 16),
+			m:     make(map[LayerIndex]*drawLayerDrawer),
+		}
 		sys.Set("layers", layers)
 		sys.View().SetOnEntityAdded(func(e ecs.Entity, w *ecs.World) {
 			// entity added to the system's view
-			layers := sys.Get("layers").(drawLayerDrawers)
+			layers := sys.Get("layers").(*drawLayerDrawers)
 			lcomp := w.Component(CNDrawLayer).Data(e).(*DrawLayer)
 			lcomp.prevLayer = lcomp.Layer
-			l := layers.Find(lcomp.Layer)
-			if l == nil {
-				l = newDrawLayerDrawer(lcomp.Layer)
-				layers = append(layers, l)
-				sort.Sort(layers)
-				sys.Set("layers", layers)
-			}
+			l := layers.UpsertLayer(lcomp.Layer)
 			if lcomp.ZIndex == ZIndexBottom {
-				if lv := l.items.FirstValue(); lv != nil {
+				if lv := l.FirstValue(); lv != nil {
 					lcomp.ZIndex = lv.(*drawLayerItemCache).ZIndex - 1
 				} else {
 					lcomp.ZIndex = 0
 				}
 			}
 			if lcomp.ZIndex == ZIndexTop {
-				if lv := l.items.LastValue(); lv != nil {
+				if lv := l.LastValue(); lv != nil {
 					lcomp.ZIndex = lv.(*drawLayerItemCache).ZIndex + 1
 				} else {
 					lcomp.ZIndex = 0
 				}
 			}
 			lcomp.prevIndex = lcomp.ZIndex
-			_ = l.items.AddOrUpdate(e, &drawLayerItemCache{
+			_ = l.AddOrUpdate(e, &drawLayerItemCache{
 				ZIndex: lcomp.ZIndex,
 			})
 		})
 		sys.View().SetOnEntityRemoved(func(e ecs.Entity, w *ecs.World) {
 			// entity removed from the system's view
-			layers := sys.Get("layers").(drawLayerDrawers)
+			layers := sys.Get("layers").(*drawLayerDrawers)
 			lcomp := w.Component(CNDrawLayer).Data(e).(*DrawLayer)
-			if l := layers.Find(lcomp.Layer); l != nil {
-				_ = l.items.Delete(e)
+			if l := layers.UpsertLayer(lcomp.Layer); l != nil {
+				_ = l.Delete(e)
 			}
 			if lcomp.prevLayer != lcomp.Layer {
-				if l := layers.Find(lcomp.prevLayer); l != nil {
-					_ = l.items.Delete(e)
+				if l := layers.UpsertLayer(lcomp.prevLayer); l != nil {
+					_ = l.Delete(e)
 				}
 			}
 		})
@@ -190,31 +214,26 @@ func DrawLayerSystemExec(ctx Context) {
 	// dt float64, v *ecs.View, s *ecs.System
 	s := ctx.System()
 	v := s.View()
-	layers := s.Get("layers").(drawLayerDrawers)
+	layers := s.Get("layers").(*drawLayerDrawers)
 	for _, match := range v.Matches() {
 		dlc := match.Components[s.World().Component(CNDrawLayer)].(*DrawLayer)
 		if dlc.Layer != dlc.prevLayer {
 			// switch layers
-			if l := layers.Find(dlc.prevLayer); l != nil {
-				_ = l.items.Delete(match.Entity)
+			if l := layers.UpsertLayer(dlc.prevLayer); l != nil {
+				_ = l.Delete(match.Entity)
 			}
 			dlc.prevLayer = dlc.Layer
 			//
-			l := layers.Find(dlc.Layer)
-			if l == nil {
-				l = newDrawLayerDrawer(dlc.Layer)
-				layers = append(layers, l)
-				s.Set("layers", layers)
-			}
+			l := layers.UpsertLayer(dlc.Layer)
 			// update index history since the layer changed anyway
 			dlc.prevIndex = dlc.ZIndex
-			l.items.AddOrUpdate(match.Entity, &drawLayerItemCache{
+			l.AddOrUpdate(match.Entity, &drawLayerItemCache{
 				ZIndex: dlc.ZIndex,
 			})
 		} else if dlc.ZIndex != dlc.prevIndex {
 			dlc.prevIndex = dlc.ZIndex
-			if l := layers.Find(dlc.Layer); l != nil {
-				l.items.AddOrUpdate(match.Entity, &drawLayerItemCache{
+			if l := layers.UpsertLayer(dlc.Layer); l != nil {
+				l.AddOrUpdate(match.Entity, &drawLayerItemCache{
 					ZIndex: dlc.ZIndex,
 				})
 			}
@@ -264,11 +283,11 @@ func (cs *DrawLayerSpriteComponentSystem) Components(w *ecs.World) []*ecs.Compon
 func DrawLayerSpriteSystemExec(ctx Context) {
 	world := ctx.World()
 	screen := ctx.Screen()
-	layers := world.System(SNDrawLayer).Get("layers").(drawLayerDrawers)
+	layers := world.System(SNDrawLayer).Get("layers").(*drawLayerDrawers).All()
 	spritec := world.Component(CNSprite)
 	defaultopts := world.Get(DefaultImageOptions).(*ebiten.DrawImageOptions)
 	for _, layer := range layers {
-		layer.items.Each(func(key ecs.Entity, value interface{}) bool {
+		layer.Items.Each(func(key ecs.Entity, value SLVal) bool {
 			cache := value.(*drawLayerItemCache)
 			if cache.Sprite == nil {
 				cache.Sprite = spritec.Data(key).(*Sprite)
--- sortedlist.go
+++ sortedlist.go
@@ -7,193 +7,32 @@ import (
 	"github.com/gabstv/ecs"
 )
 
-type SortedListLess func(ival, jval interface{}) bool
-
-// SortedListCompare should return TRUE if haystackitemval is less or equal to needleval
-type SortedListCompare func(needleval, haystackitemval interface{}) bool
-
-type IntSortedList struct {
-	l         sync.RWMutex
-	set       map[int64]*intSortedListEntry
-	list      []*intSortedListEntry
-	lessfn    SortedListLess
-	comparefn SortedListCompare
-	mincap    int
+type SLVal interface {
+	Less(v interface{}) bool
 }
-
-type intSortedListEntry struct {
-	Key   int64
-	Value interface{}
-}
-
-func NewIntSortedList(less SortedListLess, comp SortedListCompare, mincap int) *IntSortedList {
-	return &IntSortedList{
-		set:       make(map[int64]*intSortedListEntry),
-		list:      make([]*intSortedListEntry, 0, mincap),
-		lessfn:    less,
-		comparefn: comp,
-		mincap:    mincap,
-	}
-}
-
-func (sl *IntSortedList) AddOrUpdate(key int64, value interface{}) bool {
-	sl.l.RLock()
-	e, ok := sl.set[key]
-	sl.l.RUnlock()
-	if ok {
-		if e.Value == value {
-			return false
-		}
-		// update and sort
-		e.Value = value
-		sl.sort()
-		return false
-	}
-	item := &intSortedListEntry{
-		Key:   key,
-		Value: value,
-	}
-	sl.l.Lock()
-	sl.set[key] = item
-	sl.list = append(sl.list, item)
-	sl.sort()
-	sl.l.Unlock()
-	return true
-}
-
-// Find returns the key and value of the item.
-func (sl *IntSortedList) Find(val interface{}) (int64, bool) {
-	sl.l.RLock()
-	defer sl.l.RUnlock()
-	n := sort.Search(len(sl.list), func(i int) bool {
-		return sl.comparefn(val, sl.list[i].Value)
-	})
-	if n == len(sl.list) {
-		return 0, false
-	}
-	if sl.list[n].Value != val {
-		return 0, false
-	}
-	return sl.list[n].Key, true
-}
-
-func (sl *IntSortedList) Get(key int64) (interface{}, bool) {
-	sl.l.RLock()
-	defer sl.l.RUnlock()
-	if v, ok := sl.set[key]; ok {
-		return v.Value, true
-	}
-	return nil, false
-}
-
-func (sl *IntSortedList) Delete(key int64) bool {
-	sl.l.Lock()
-	defer sl.l.Unlock()
-	vv, ok := sl.set[key]
-	if !ok {
-		return false
-	}
-	n := sort.Search(len(sl.list), func(i int) bool {
-		return sl.comparefn(vv.Value, sl.list[i].Value)
-	})
-	if n == len(sl.list) {
-		return false
-	}
-	if sl.list[n].Key != key {
-		return false
-	}
-	sl.list = append(sl.list[:n], sl.list[n+1:]...)
-	delete(sl.set, key)
-	return true
-}
-
-func (sl *IntSortedList) LastValue() interface{} {
-	sl.l.RLock()
-	defer sl.l.RUnlock()
-	if len(sl.list) < 1 {
-		return nil
-	}
-	return sl.list[len(sl.list)-1].Value
-}
-
-func (sl *IntSortedList) FirstValue() interface{} {
-	sl.l.RLock()
-	defer sl.l.RUnlock()
-	if len(sl.list) < 1 {
-		return nil
-	}
-	return sl.list[0].Value
-}
-
-// Reset clears the list.
-func (sl *IntSortedList) Reset() {
-	sl.l.Lock()
-	defer sl.l.Unlock()
-	sl.set = make(map[int64]*intSortedListEntry)
-	sl.list = make([]*intSortedListEntry, 0, sl.mincap)
-}
-
-// Each iterates on all entries, sorted.
-//
-// Do not delete or add entries while looping (it is read locked while looping).
-func (sl *IntSortedList) Each(fn func(key int64, value interface{}) bool) {
-	sl.l.RLock()
-	defer sl.l.RUnlock()
-	for _, v := range sl.list {
-		if !fn(v.Key, v.Value) {
-			return
-		}
-	}
-}
-
-func (sl *IntSortedList) sort() {
-	sort.Slice(sl.list, sl.sortfn())
-}
-
-func (sl *IntSortedList) sortfn() func(i, j int) bool {
-	l := sl.list
-	return func(i, j int) bool {
-		ie := l[i]
-		je := l[j]
-		return sl.lessfn(ie.Value, je.Value)
-	}
-}
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 type EntitySortedList struct {
-	l         sync.RWMutex
-	set       map[ecs.Entity]*entitySortedListEntry
-	list      []*entitySortedListEntry
-	lessfn    SortedListLess
-	comparefn SortedListCompare
-	mincap    int
+	l      sync.RWMutex
+	set    map[ecs.Entity]*entitySortedListEntry
+	list   []*entitySortedListEntry
+	mincap int
 }
 
 type entitySortedListEntry struct {
 	Key   ecs.Entity
-	Value interface{}
+	Value SLVal
+	Index int
 }
 
-func NewEntitySortedList(less SortedListLess, comp SortedListCompare, mincap int) *EntitySortedList {
+func NewEntitySortedList(mincap int) *EntitySortedList {
 	return &EntitySortedList{
-		set:       make(map[ecs.Entity]*entitySortedListEntry),
-		list:      make([]*entitySortedListEntry, 0, mincap),
-		lessfn:    less,
-		comparefn: comp,
-		mincap:    mincap,
+		set:    make(map[ecs.Entity]*entitySortedListEntry),
+		list:   make([]*entitySortedListEntry, 0, mincap),
+		mincap: mincap,
 	}
 }
 
-func (sl *EntitySortedList) AddOrUpdate(key ecs.Entity, value interface{}) bool {
+func (sl *EntitySortedList) AddOrUpdate(key ecs.Entity, value SLVal) bool {
 	sl.l.RLock()
 	e, ok := sl.set[key]
 	sl.l.RUnlock()
@@ -203,14 +42,17 @@ func (sl *EntitySortedList) AddOrUpdate(key ecs.Entity, value interface{}) bool 
 		}
 		// update and sort
 		e.Value = value
+		sl.l.Lock()
 		sl.sort()
+		sl.l.Unlock()
 		return false
 	}
+	sl.l.Lock()
 	item := &entitySortedListEntry{
 		Key:   key,
 		Value: value,
+		Index: len(sl.list),
 	}
-	sl.l.Lock()
 	sl.set[key] = item
 	sl.list = append(sl.list, item)
 	sl.sort()
@@ -218,23 +60,7 @@ func (sl *EntitySortedList) AddOrUpdate(key ecs.Entity, value interface{}) bool 
 	return true
 }
 
-// Find returns the key and value of the item.
-func (sl *EntitySortedList) Find(val interface{}) (ecs.Entity, bool) {
-	sl.l.RLock()
-	defer sl.l.RUnlock()
-	n := sort.Search(len(sl.list), func(i int) bool {
-		return sl.comparefn(val, sl.list[i].Value)
-	})
-	if n == len(sl.list) {
-		return 0, false
-	}
-	if sl.list[n].Value != val {
-		return 0, false
-	}
-	return sl.list[n].Key, true
-}
-
-func (sl *EntitySortedList) Get(key ecs.Entity) (interface{}, bool) {
+func (sl *EntitySortedList) Get(key ecs.Entity) (SLVal, bool) {
 	sl.l.RLock()
 	defer sl.l.RUnlock()
 	if v, ok := sl.set[key]; ok {
@@ -250,21 +76,15 @@ func (sl *EntitySortedList) Delete(key ecs.Entity) bool {
 	if !ok {
 		return false
 	}
-	n := sort.Search(len(sl.list), func(i int) bool {
-		return sl.comparefn(vv.Value, sl.list[i].Value)
-	})
-	if n == len(sl.list) {
-		return false
+	for i := vv.Index + 1; i < len(sl.list); i++ {
+		sl.list[i].Index = i - 1
 	}
-	if sl.list[n].Key != key {
-		return false
-	}
-	sl.list = append(sl.list[:n], sl.list[n+1:]...)
+	sl.list = append(sl.list[:vv.Index], sl.list[vv.Index+1:]...)
 	delete(sl.set, key)
 	return true
 }
 
-func (sl *EntitySortedList) LastValue() interface{} {
+func (sl *EntitySortedList) LastValue() SLVal {
 	sl.l.RLock()
 	defer sl.l.RUnlock()
 	if len(sl.list) < 1 {
@@ -273,7 +93,7 @@ func (sl *EntitySortedList) LastValue() interface{} {
 	return sl.list[len(sl.list)-1].Value
 }
 
-func (sl *EntitySortedList) FirstValue() interface{} {
+func (sl *EntitySortedList) FirstValue() SLVal {
 	sl.l.RLock()
 	defer sl.l.RUnlock()
 	if len(sl.list) < 1 {
@@ -293,7 +113,7 @@ func (sl *EntitySortedList) Reset() {
 // Each iterates on all entries, sorted.
 //
 // Do not delete or add entries while looping (it is read locked while looping).
-func (sl *EntitySortedList) Each(fn func(key ecs.Entity, value interface{}) bool) {
+func (sl *EntitySortedList) Each(fn func(key ecs.Entity, value SLVal) bool) {
 	sl.l.RLock()
 	defer sl.l.RUnlock()
 	for _, v := range sl.list {
@@ -304,14 +124,18 @@ func (sl *EntitySortedList) Each(fn func(key ecs.Entity, value interface{}) bool
 }
 
 func (sl *EntitySortedList) sort() {
-	sort.Slice(sl.list, sl.sortfn())
+	sort.Sort(entitySortedListSt(sl.list))
 }
 
-func (sl *EntitySortedList) sortfn() func(i, j int) bool {
-	l := sl.list
-	return func(i, j int) bool {
-		ie := l[i]
-		je := l[j]
-		return sl.lessfn(ie.Value, je.Value)
-	}
+type entitySortedListSt []*entitySortedListEntry
+
+func (a entitySortedListSt) Len() int { return len(a) }
+func (a entitySortedListSt) Swap(i, j int) {
+	a[i].Index, a[j].Index = j, i
+	a[i], a[j] = a[j], a[i]
+}
+func (a entitySortedListSt) Less(i, j int) bool {
+	a[i].Index = i
+	a[j].Index = j
+	return a[i].Value.Less(a[j].Value)
 }
