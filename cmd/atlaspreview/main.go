1230a9d839d07bc15359f6dee816593d519ff307
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-06-05 11:36:57 -0300

    progress

--- cmd/atlaspreview/main.go
+++ cmd/atlaspreview/main.go
@@ -116,7 +116,7 @@ func (p *AtlasPreviewer) createCanvas() {
 func (p *AtlasPreviewer) setupAnim(name string) {
 	p.resetCanvas()
 	p.createCanvas()
-	p.canvas.SetScale2(2 * ebiten.DeviceScaleFactor())
+	p.canvas.SetScale2(8 * ebiten.DeviceScaleFactor())
 	anim := p.atlas.GetAnimation(name)
 	title := primen.NewLabel(p.titler, nil, uiLayer)
 	title.SetText("Animation: " + name)
